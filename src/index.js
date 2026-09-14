2aea439f0fce3bea82dd570d0d3b1f960004df1f
jiajun1014 <wu0937822067@gmail.com>
2026-09-14 18:12:45 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -40,23 +40,23 @@ function isAdmin(request, env) {
 // =========================================================
 
 const CHECKIN_SCHEDULE = {
-  1: { start: "09:00", end: "20:00" }, // 星期一
+  1: { start: "09:00", end: "10:00" }, // 星期一
   2: { start: "09:00", end: "10:00" }, // 星期二
   4: { start: "09:00", end: "10:00" }  // 星期四
 };
 
 // 簽到地點：輔仁大學
-// const CHECKIN_AREA = {
-//   latitude: 25.033649,
-//   longitude: 121.433255,
-//   radiusMeters: 200
-// };
-// 簽到地點：國立中興大學
 const CHECKIN_AREA = {
-  latitude: 24.123806,
-  longitude: 120.675194,
+  latitude: 25.033649,
+  longitude: 121.433255,
   radiusMeters: 200
 };
+// 簽到地點：國立中興大學
+// const CHECKIN_AREA = {
+//   latitude: 24.123806,
+//   longitude: 120.675194,
+//   radiusMeters: 200
+// };
 
 function distanceMeters(lat1, lon1, lat2, lon2) {
   const R = 6371000;
